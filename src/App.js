a35f81bd33beff71cf6d045e75f2c20d07dbaa5d
patspringer <pspringer10@gmail.com>
2021-08-12 14:31:06 -0400

    cool

--- src/App.js
+++ src/App.js
@@ -13,12 +13,6 @@ function App() {
 }
   return (
     <>
-      <GoogleLogin
-    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-  />,
       <Router>
         <Navbar />
         <Switch>
